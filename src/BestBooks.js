9b70c0521dea8af0ff58aef3805458261228b707
Stacy Yu <stacyyuu@gmail.com>
2022-11-02 19:50:33 -0700

    lab13

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -3,8 +3,8 @@ import axios from 'axios';
 import Carousel from 'react-bootstrap/Carousel';
 import AddABookForm from './AddABookForm';
 import Button from 'react-bootstrap/Button';
+import UpdateABookForm from './UpdateABookForm';
 
-// import { carousel } from 'react-responsive-carousel';
 
 class BestBooks extends React.Component {
   constructor(props) {
@@ -12,7 +12,9 @@ class BestBooks extends React.Component {
     this.state = {
       books: [],
       showNewBookForm: false,
-      errorMessage: ''
+      errorMessage: '',
+      selectedBook: {},
+      show: false
     }
     console.log(this.state.books);
   }
@@ -20,7 +22,7 @@ class BestBooks extends React.Component {
   componentDidMount = async () => {
     const config = {
       method: 'get', // get is the default
-      baseURL: 'http://localhost:3001',
+      baseURL: process.env.HEROKU_APP,
       url: '/books' // endpoint
     }
     const response = await axios(config);
@@ -32,85 +34,119 @@ class BestBooks extends React.Component {
     try {
       const config = {
         method: 'post',
-        baseURL: 'http://localhost:3001',
+        baseURL: process.env.HEROKU_APP,
         url: '/books',
         data: bookToBeCreated
       }
       const res = await axios(config);
       this.setState({ books: [...this.state.books, res.data] })
-    } catch(error) {
+    } catch (error) {
       console.error('Error is in the App.js in the newBook Function:', error);
-      this.setState({ errorMessage: `Status Code ${error.response.status}: ${error.response.data}`});
+      this.setState({ errorMessage: `Status Code ${error.response.status}: ${error.response.data}` });
     }
   }
-  
+
   handleDeleteBook = async (bookToBeDeleted) => {
     try {
       const proceed = window.confirm(`Do you wish to delete ${bookToBeDeleted.title}?`);
-  
+
       if (proceed) {
         const config = {
           method: 'delete',
-          baseURL: 'http://localhost:3001',
+          baseURL: process.env.HEROKU_APP,
           url: `/books/${bookToBeDeleted._id}`
         }
-  
+
         const res = await axios(config);
         console.log(res.data);
         const newBooksArr = this.state.books.filter(book => book._id !== bookToBeDeleted._id);
         this.setState({ books: newBooksArr });
       }
-    } catch(error) {
+    } catch (error) {
       console.error('Error is in the App.js in the deleteBook Function: ', error);
       // axios sends more info about the error in a response object on the error
-      this.setState({ errorMessage: `Status Code ${error.response.status}: ${error.response.data}`});
+      this.setState({ errorMessage: `Status Code ${error.response.status}: ${error.response.data}` });
     }
   }
 
-  showForm = () => this.setState({ showNewBookForm: true });
+  handleUpdateBook = async (bookToBeUpdated) => {
+    try {
+      const config = {
+        method: 'put',
+        baseURL: process.env.HEROKU_APP,
+        url: `/books/${bookToBeUpdated._id}`
+      }
+      const res = await axios(config);
+      console.log(res.data);
+      const updatedBooks = this.state.books.map(preExistingBook => {
+        if (preExistingBook._id === bookToBeUpdated._id) {
+          return bookToBeUpdated;
+        } else {
+          return preExistingBook;
+        }})
+        this.setState({ books: updatedBooks });
+      } catch (error) {
+        console.error('Error is in the App.js in the updateBook Function: ', error);
+        // axios sends more info about the error in a response object on the error
+        this.setState({ errorMessage: `Status Code ${error.response.status}: ${error.response.data}` });
+      }
+    }
 
-  render() {
+    showForm = () => this.setState({ showNewBookForm: true });
+    handleSelectBook = (bookToBeSelected) => this.setState({ selectedBook: bookToBeSelected, show: true });
+    handleOnHide = () => this.setState({ selectedBook: {}, show: false })
 
-    /* TODO: render all the books in a Carousel */
+    render() {
 
-    return (
-      <>
-        <h2>Neat Books</h2>
+      /* TODO: render all the books in a Carousel */
 
-        <Button onClick={this.showForm}>Add a New Book!</Button>
+      return (
+        <>
+          <h2>Neat Books</h2>
 
-      {this.state.showNewBookForm && 
-        <AddABookForm 
-          handleCreateBook={this.handleCreateBook}
-        /> }
+          <Button onClick={this.showForm}>Add a New Book!</Button>
 
-        <Carousel>
-          {this.state.books.length !== 0 ? (this.state.books.map(book =>
-          (
+          {this.state.showNewBookForm &&
+            <AddABookForm
+              handleCreateBook={this.handleCreateBook}
+            />}
 
-            <Carousel.Item key={book._id}>
-              <img
-                className="d-block w-100"
-                src="https://images.unsplash.com/photo-1497633762265-9d179a990aa6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1473&q=80"
-                alt="Second slide"
-              />
+          {this.state.selectedBook.title &&
+            <UpdateABookForm
+              selectedBook={this.state.selectedBook}
+              show={this.state.show}
+              handleOnHide={this.handleOnHide}
+              updateBook={this.handleUpdateBook}
+            />}
 
-              <Carousel.Caption>
-                <h3>{book.title}</h3>
-                <p>{book.description}</p>
-                <p>{book.status}</p>
-                <Button onClick={() => this.handleDeleteBook(book)}>Delete this book!</Button>
-              </Carousel.Caption>
-            </Carousel.Item>
+          <Carousel>
+            {this.state.books.length !== 0 ? (this.state.books.map(book =>
+            (
 
-          )))
-            :
-            <h2>{this.state.errorMessage}</h2>
-          }
-        </Carousel>
-      </>
-    )
+              <Carousel.Item key={book._id}>
+                <img
+                  className="d-block w-100"
+                  src="https://images.unsplash.com/photo-1497633762265-9d179a990aa6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1473&q=80"
+                  alt="Second slide"
+                />
+
+                <Carousel.Caption>
+                  <h3>{book.title}</h3>
+                  <p>{book.description}</p>
+                  <p>{book.status}</p>
+                  <Button onClick={() => this.handleDeleteBook(book)}>Delete this book!</Button>
+                  <Button onClick={() => this.handleSelectBook(book)}>Update this book!</Button>
+                </Carousel.Caption>
+              </Carousel.Item>
+
+            )))
+              :
+              <h2>{this.state.errorMessage}</h2>
+            }
+          </Carousel>
+        </>
+      )
+    }
   }
-}
 
 export default BestBooks;
